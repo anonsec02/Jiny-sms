c51630b69e8d48a8444e6ca25c05840c34efaa5e
anonsec02 <yalaomamertaz@gmail.com>
2025-04-13 12:23:31 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const request = require("request");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
 const app = express();
 
-app.use("/", (req, res) => {
-  const url = "http://82.151.73.56:8800" + req.url;
-  req.pipe(request({ url })).pipe(res);
-});
+app.use("/", createProxyMiddleware({
+  target: "http://82.151.73.56:8800",
+  changeOrigin: true,
+  ws: true, // لدعم WebSockets إذا كانت مطلوبة
+  onError(err, req, res) {
+    res.writeHead(500, {
+      "Content-Type": "text/plain"
+    });
+    res.end("حدث خطأ في الاتصال بالخادم الهدف.");
+  }
+}));
 
 app.listen(3000, () => {
   console.log("Proxy server running on port 3000");
